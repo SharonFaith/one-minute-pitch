bd714257c5347babf9b7f1edc30f409f58a69225
SharonFaith <sharonfaith15@gmail.com>
2020-10-22 23:23:48 +0300

    import login form

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for
 from . import auth
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from .. import db
 from flask_login import login_user, logout_user, login_required
 
