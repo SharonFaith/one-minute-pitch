28d5153821912f6784455a54b7cc73b25035ef21
SharonFaith <sharonfaith15@gmail.com>
2020-10-23 00:04:14 +0300

    add update profile form

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,6 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Required
 
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.', validators=[Required()])
+    submit = SubmitField('Submit')
+
 
 class DeleteUser(FlaskForm):
     submit = SubmitField('Delete account')
