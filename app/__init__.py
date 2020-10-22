53397ac9ca2124ffc48bcb9a23023f9eb2029346
SharonFaith <sharonfaith15@gmail.com>
2020-10-23 00:40:26 +0300

    initialize flask  mail extension

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -12,7 +13,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos', IMAGES)
-
+main = Mail()
 
 def create_app(config_name):
 
@@ -25,6 +26,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     #configure UploadSet
     configure_uploads(app, photos)
