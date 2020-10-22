433a31e9d517b43b079ffbcafa395f49b3b5e517
SharonFaith <sharonfaith15@gmail.com>
2020-10-22 23:13:57 +0300

    add login view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,9 +4,19 @@ from ..models import User
 from .forms import RegistrationForm
 from .. import db
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or Password')
+
+    title = 'one min app login'
+
+    return render_template('auth/login.html', login_form = login_form, title = title)
 
 
 @auth.route('/register', methods = ['GET', 'POST'])
@@ -17,7 +27,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to watchlist", "email/welcome_user", user.email, user = user)
+        
         return redirect(url_for('auth.login'))
         title = 'New Account'
     
