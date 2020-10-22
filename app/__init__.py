2f0bf36b62824ba6d2120503e5a9490db9c5ecb5
SharonFaith <sharonfaith15@gmail.com>
2020-10-23 00:42:04 +0300

    fix bug

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos', IMAGES)
-main = Mail()
+mail = Mail()
 
 def create_app(config_name):
 
