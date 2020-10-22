186217bf6f3de7916eff31bc8014f4611d6918f4
SharonFaith <sharonfaith15@gmail.com>
2020-10-22 23:52:33 +0300

    add view function for user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, abort
 from . import main
+from ..models import Reviews, User
+from .forms import DeleteUser
 
 
 
@@ -10,3 +12,21 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+
+@main.route('/user/<uname>', methods = ['GET', 'POST'])
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    form = DeleteUser()
+    
+
+    if user is None:
+        abort(404)
+    elif form.validate_on_submit():
+        db.session.delete(user)
+        db.session.commit()
+
+        return redirect(url_for('main.index'))
+    else:
+
+        return render_template('profile/profile.html', user = user, delete = form)
